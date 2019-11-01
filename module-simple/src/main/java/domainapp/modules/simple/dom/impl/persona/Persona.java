22f3090d22def71b8c41aa7cc5e0533bdaf30e0a
cintiaMillacura2207 <cg2207m@gmail.com>
2019-11-01 14:10:06 -0300

    Issue #3:Se agrego metodo eliminar
      Se agrego el metodo que permite eliminar una entidad de dominio
      Persona.

--- module-simple/src/main/java/domainapp/modules/simple/dom/impl/persona/Persona.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/persona/Persona.java
@@ -20,10 +20,15 @@ import org.apache.isis.applib.annotation.Property;
 import org.apache.isis.applib.annotation.Publishing;
 import org.apache.isis.applib.annotation.Title;
 import org.apache.isis.applib.services.i18n.TranslatableString;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.services.title.TitleService;
 
 import domainapp.modules.simple.dom.impl.enums.TipoJerarquia;
+import lombok.AccessLevel;
 import static org.apache.isis.applib.annotation.CommandReification.ENABLED;
 import static org.apache.isis.applib.annotation.SemanticsOf.IDEMPOTENT;
+import static org.apache.isis.applib.annotation.SemanticsOf.NON_IDEMPOTENT_ARE_YOU_SURE;
 
 @PersistenceCapable(
         identityType = IdentityType.DATASTORE,
@@ -297,6 +302,16 @@ public class Persona implements Comparable<Persona> {
         return telefono != null && telefono.contains("!") ? TranslatableString.tr("Exclamation mark is not allowed") : null;
     }
 
+    /**
+     * Este metodo permite eliminar la entidad de Persona
+     */
+    @Action(semantics = NON_IDEMPOTENT_ARE_YOU_SURE)
+    public void delete() {
+        final String title = titleService.titleOf(this);
+        messageService.informUser(String.format("'%s' FUE ELIMINADA ESTA PERSONA", title));
+        repositoryService.remove(this);
+    }
+
     //region > compareTo, toString
     @Override
     public int compareTo(final Persona other) {
@@ -308,5 +323,21 @@ public class Persona implements Comparable<Persona> {
         return org.apache.isis.applib.util.ObjectContracts.toString(this, "nombre");
     }
     //endregion
+
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    RepositoryService repositoryService;
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    TitleService titleService;
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    MessageService messageService;
 
 }
