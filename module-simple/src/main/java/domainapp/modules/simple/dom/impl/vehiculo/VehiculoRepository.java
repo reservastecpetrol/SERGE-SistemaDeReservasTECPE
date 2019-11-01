ee55290c4f3a37a91306790845ec5b905034f013
cintiaMillacura2207 <cg2207m@gmail.com>
2019-11-01 16:11:52 -0300

    Issue #24:Se agrego funcionalidad
      Se agrego el metodo que permite listar todos los vehiculos registrados
      en el sistema.

--- module-simple/src/main/java/domainapp/modules/simple/dom/impl/vehiculo/VehiculoRepository.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/vehiculo/VehiculoRepository.java
@@ -1,20 +1,55 @@
 package domainapp.modules.simple.dom.impl.vehiculo;
 
+import java.util.List;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
 import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.NatureOfService;
 import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
+
+import lombok.AccessLevel;
 
 @DomainService(
-        nature = NatureOfService.DOMAIN,
+        nature = NatureOfService.VIEW_MENU_ONLY,
+        objectType = "simple.VehiculoMenu",
         repositoryFor = Vehiculo.class
 )
+@DomainServiceLayout(
+        named = "Vehiculos",
+        menuOrder = "10"
+)
+
+/**
+ *
+ * Esta clase es el servicio de dominio de la clase Vehiculo
+ * que define los metodos
+ * que van a aparecer en el menu del sistema
+ *
+ *@author Cintia Millacura
+ *
+ */
 public class VehiculoRepository {
 
-    @Programmatic
-    public java.util.List<Vehiculo> listAll() {
-        return container.allInstances(Vehiculo.class);
+    /**
+     * Este metodo lista todos los Vehiculos que hay cargados
+     * en el sistema
+     *
+     * @return List<Vehiculo>
+     */
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
+    @MemberOrder(sequence = "1")
+    public List<Vehiculo> listarVehiculos() {
+        return repositoryService.allInstances(Vehiculo.class);
     }
-
     @Programmatic
     public Vehiculo findByMatricula(
             final String matricula
@@ -55,6 +90,17 @@ public class VehiculoRepository {
         }
         return vehiculo;
     }
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    MessageService messageService;
+
+    @javax.inject.Inject
+    RepositoryService repositoryService;
+
+    @javax.inject.Inject
+    IsisJdoSupport isisJdoSupport;
 
     @javax.inject.Inject
     org.apache.isis.applib.DomainObjectContainer container;
