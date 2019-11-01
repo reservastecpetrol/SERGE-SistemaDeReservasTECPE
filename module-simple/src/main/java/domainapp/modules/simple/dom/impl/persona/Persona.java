6b72f6e2f4b2ecf4bc08cb71b2b4d04d80cd2815
cintiaMillacura2207 <cg2207m@gmail.com>
2019-11-01 13:51:21 -0300

    Issue #1:Se agregaron propiedades
      Se agregaron las propiedades que definen la entidad de dominio
      Persona.

--- module-simple/src/main/java/domainapp/modules/simple/dom/impl/persona/Persona.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/persona/Persona.java
@@ -1,15 +1,26 @@
 package domainapp.modules.simple.dom.impl.persona;
 
-import org.apache.isis.applib.annotation.*;
+import javax.jdo.annotations.DatastoreIdentity;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Queries;
+import javax.jdo.annotations.Query;
+import javax.jdo.annotations.Version;
+import javax.jdo.annotations.VersionStrategy;
 
-import javax.jdo.annotations.*;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.Editing;
+import org.apache.isis.applib.annotation.Property;
+import org.apache.isis.applib.annotation.Title;
 
-import lombok.Getter;
-import lombok.Setter;
+import domainapp.modules.simple.dom.impl.enums.TipoJerarquia;
 
 @PersistenceCapable(
         identityType = IdentityType.DATASTORE,
-        schema = "persona",
+        schema = "simple",
         table = "Persona"
 )
 @DatastoreIdentity(
@@ -34,20 +45,88 @@ import lombok.Setter;
                         + "FROM domainapp.modules.simple.dom.impl.persona.Persona "
                         + "WHERE nombre == :nombre ")
 })
-@Unique(name = "Persona_nombre_UNQ", members = { "nombre" })
+//Se comenta de forma que se pueda ingresar indefinidamente el mismo nombre de un Usuario del sistema
+//@Unique(name = "Persona_nombre_UNQ", members = { "nombre" })
 @DomainObject(
         editing = Editing.DISABLED
 )
 @DomainObjectLayout(
         bookmarking = BookmarkPolicy.AS_ROOT
 )
+@lombok.Getter @lombok.Setter
+/**
+ * Esta clase define la entidad de dominio Persona que hace referencia al Usuario del sistema definiendo asi
+ * todas sus propiedades asi tambien sus metodos constructores,de validacion de variables,actualizacion de variables
+ * y ademas poder relizar la accion de eliminar una entidad Persona.
+ *
+ * @author Francisco Bellani
+ *
+ */
+
 public class Persona implements Comparable<Persona> {
 
-    @Column(allowsNull = "false")
-    @Property()
-    @Getter @Setter
-    private String nombre;
+    //Se defininen todas las propiedades de la entidad de dominio Persona
 
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property() // editing disabled by default, see isis.properties
+    @Title(prepend = "Persona: ")
+    private String nombre; //esta variable hace referencia al nombre de la entidad Persona
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property() // editing disabled by default, see isis.properties
+    @Title()
+    private String apellido; //esta variable hace referencia al apellido de la entidad Persona
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private String direccion; //esta variable hace referencia a la dirección de la entidad Persona
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length =10)
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private String telefono; //esta variable hace referencia al telefono de la entidad Persona
+
+    @javax.jdo.annotations.Column()
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private String email; //esta variable hace referencia a la direccion de email de la entidad Persona
+
+    @javax.jdo.annotations.Column(allowsNull ="false", length = 8)
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private String dni; //esta variable hace referencia al numero de documento (DNI) de la entidad Persona
+
+    //listado de Jerarquias dropdown menu
+    @javax.jdo.annotations.Column(allowsNull="false")
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private TipoJerarquia jerarquia; //esta variable hace referencia al tipo de jerarquia de la entidad Persona
+
+    public Persona(){}
+
+    /**
+     * Este es un metodo constructor
+     *
+     * @param nombre
+     * @param apellido
+     * @param direccion
+     * @param telefono
+     * @param email
+     * @param dni
+     * @param jerarquia
+     */
+    public Persona(String nombre,String apellido,String direccion,String telefono,String email,String dni,TipoJerarquia jerarquia){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.direccion=direccion;
+        this.telefono=telefono;
+        this.email=email;
+        this.dni=dni;
+        this.jerarquia=jerarquia;
+    }
     //region > compareTo, toString
     @Override
     public int compareTo(final Persona other) {
