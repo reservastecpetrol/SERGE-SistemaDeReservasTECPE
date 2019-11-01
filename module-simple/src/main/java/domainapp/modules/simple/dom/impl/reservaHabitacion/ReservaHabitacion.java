ae303c6330d9bdd7eb5e1334ca39ec06effe44d6
cintiaMillacura2207 <cg2207m@gmail.com>
2019-11-01 15:39:16 -0300

    Issue #30:Se agrego propiedades y constructores
      Se agregaron las propiedades de la entidad y sus metodos
      constructores.

--- module-simple/src/main/java/domainapp/modules/simple/dom/impl/reservaHabitacion/ReservaHabitacion.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/reservaHabitacion/ReservaHabitacion.java
@@ -1,13 +1,12 @@
 package domainapp.modules.simple.dom.impl.reservaHabitacion;
 
-import javax.jdo.annotations.Column;
 import javax.jdo.annotations.DatastoreIdentity;
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.Queries;
 import javax.jdo.annotations.Query;
-import javax.jdo.annotations.Unique;
 import javax.jdo.annotations.Version;
 import javax.jdo.annotations.VersionStrategy;
 
@@ -18,14 +17,17 @@ import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.DomainObjectLayout;
 import org.apache.isis.applib.annotation.Editing;
 import org.apache.isis.applib.annotation.Property;
+import org.apache.isis.applib.annotation.Title;
 
-import lombok.Getter;
-import lombok.Setter;
+import domainapp.modules.simple.dom.impl.enums.EstadoReserva;
+import domainapp.modules.simple.dom.impl.habitacion.Habitacion;
+import domainapp.modules.simple.dom.impl.persona.Persona;
 
 @PersistenceCapable(
         identityType = IdentityType.DATASTORE,
-        schema = "reservaHabitacion",
-        table = "ReservaHabitacion"
+        schema = "simple",
+        table = "ReservaHabitacion",
+        detachable="true"
 )
 @DatastoreIdentity(
         strategy = IdGeneratorStrategy.IDENTITY,
@@ -49,19 +51,98 @@ import lombok.Setter;
                         + "FROM domainapp.modules.simple.dom.impl.reservaHabitacion.ReservaHabitacion "
                         + "WHERE fechaReserva == :fechaReserva ")
 })
-@Unique(name = "ReservaHabitacion_fechaReserva_UNQ", members = { "fechaReserva" })
+//Se comenta de forma que permita realizar varias reservas en una misma fecha
+//@Unique(name = "ReservaVehiculo_fechaReserva_UNQ", members = { "fechaReserva" })
 @DomainObject(
         editing = Editing.DISABLED
 )
 @DomainObjectLayout(
         bookmarking = BookmarkPolicy.AS_ROOT
 )
+
+@lombok.Getter @lombok.Setter
+
+/**
+ * Esta clase define la entidad de dominio ReservaHabitacion
+ * mediante la cual un usuario puede realizar la reserva de una
+ * Habitacion dada una fecha de Inicio
+ * En la misma se definen todas sus propiedades y metodos.
+ * Entre los cuales encontramos metodos Constructores,para eliminar un objeto
+ * y metodos para modificar el estado de la reserva.
+ *
+ * @see vehiculo.Habitacion
+ * @see persona.Persona
+ *
+ *  @author Francisco Bellani
+ *
+ */
 public class ReservaHabitacion implements Comparable<ReservaHabitacion> {
 
-    @Column(allowsNull = "false")
-    @Property()
-    @Getter @Setter
-    private LocalDate fechaReserva;
+    //Definicion de las propiedades de la entidad ReservaHabitacion
+
+    @javax.jdo.annotations.Column(allowsNull = "false")
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    @Title(prepend = "Reserva: ")
+    private LocalDate fechaReserva; //esta variable hace referencia a la fecha en que se realiza la reserva
+
+    @javax.jdo.annotations.Column(allowsNull = "false")
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private LocalDate fechaInicio; //esta variable hace referencia a la fecha de inicio en la cual se va a hacer uso de la reserva
+
+    @javax.jdo.annotations.Column(allowsNull = "false")
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private LocalDate fechaFin; //esta variable hace referencia a la fecha en la que se va a dar fin a la reserva
+
+    @javax.jdo.annotations.Column(allowsNull = "false",name="PERSONA_ID")
+    @Persistent(defaultFetchGroup="true")
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private Persona persona; //esta variable hace referencia al usuario que va a hacer uso de la reserva
+
+    @javax.jdo.annotations.Column(allowsNull = "false",name="HABITACION_ID")
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    private Habitacion habitacion; //esta variable hace referencia a la Habitacion que va a ser reservada por el usuario
+
+    @javax.jdo.annotations.Column(allowsNull = "false")
+    @lombok.NonNull
+    @Property(editing = Editing.ENABLED)
+    // esta variable hace referencia al estado en el que se encuentra la reserva realizada por el usuario
+    // los cuales pueden ser :
+    //ACTIVA | CANCELADA | ARRIBADA
+    private EstadoReserva estado;
+
+
+    /**
+     * Este es un metodo constructor por defecto
+     *
+     */
+    public ReservaHabitacion(){}
+
+
+    /**
+     * Este es un metodo constructor con parametros
+     *
+     * @param fechaReserva -valor definido en el codigo
+     * @param fechaInicio -valor ingresado por el usuario
+     * @param fechaFin -valor ingresado por el usuario
+     * @param persona -valor ingresado por el usuario
+     * @param habitacion -valor ingresado por el usuario
+     * @param estado -valor definido en el codigo
+     */
+    public ReservaHabitacion(LocalDate fechaReserva,LocalDate fechaInicio,LocalDate fechaFin,
+            Persona persona, Habitacion habitacion,EstadoReserva estado){
+        this.fechaReserva=fechaReserva;
+        this.fechaInicio=fechaInicio;
+        this.fechaFin=fechaFin;
+        this.persona=persona;
+        this.habitacion=habitacion;
+        this.estado=estado;
+    }
+
 
     //region > compareTo, toString
     @Override
