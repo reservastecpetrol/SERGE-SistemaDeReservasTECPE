37f9bd847c5c751cd1e4c48ba4e9f90f5791dc2a
cintiaMillacura2207 <cg2207m@gmail.com>
2019-11-04 18:15:11 -0300

    Issue #46:Se agrego metodo en el servicio
      Se agrego el metodo que permite generar el listado de reservas de
      vehiculos dado un estado en particular.

--- module-simple/src/main/java/domainapp/modules/simple/dom/impl/reservaVehiculo/ReservaVehiculoRepository.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/reservaVehiculo/ReservaVehiculoRepository.java
@@ -1,5 +1,9 @@
 package domainapp.modules.simple.dom.impl.reservaVehiculo;
 
+import java.util.List;
+
+import org.datanucleus.query.typesafe.TypesafeQuery;
+
 import org.apache.isis.applib.annotation.Action;
 import org.apache.isis.applib.annotation.ActionLayout;
 import org.apache.isis.applib.annotation.BookmarkPolicy;
@@ -7,11 +11,14 @@ import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.DomainServiceLayout;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.SemanticsOf;
 import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
 import org.apache.isis.applib.services.message.MessageService;
 import org.apache.isis.applib.services.repository.RepositoryService;
 
+import domainapp.modules.simple.dom.impl.enums.EstadoReserva;
 import domainapp.modules.simple.dom.impl.persona.PersonaRepository;
 import domainapp.modules.simple.dom.impl.vehiculo.VehiculoRepository;
 import lombok.AccessLevel;
@@ -55,6 +62,29 @@ public class ReservaVehiculoRepository {
      */
     public java.util.List<ReservaVehiculo> listarReservasDeVehiculosTotales() {
         return container.allInstances(ReservaVehiculo.class);
+    }
+
+    
+    /**
+     * Este metodo permite recuperar en una lista todos las reservas realizadas
+     * dado un estado en particular
+     *
+     * @param estado
+     * @return List<ReservaVehiculo>
+     */
+    @Programmatic
+    public List<ReservaVehiculo> listarReservasPorEstado(
+            @ParameterLayout(named="Estado")
+            final EstadoReserva estado
+    ) {
+        TypesafeQuery<ReservaVehiculo> tq = isisJdoSupport.newTypesafeQuery(ReservaVehiculo.class);
+        final QReservaVehiculo cand = QReservaVehiculo.candidate();
+
+        List<ReservaVehiculo> reservas = tq.filter(
+                cand.estado.eq(tq.stringParameter("estado")))
+                .setParameter("estado",estado).executeList();
+
+        return reservas;
     }
 
 
